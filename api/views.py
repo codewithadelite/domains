b53467f8c79aa1eb89d0478bcbf4cdc48c8ae049
Niyonshuti Shema Adelite <shemani2011@gmail.com>
2023-06-05 16:15:36 +0200

    Filter domains implemented

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.http import HttpRequest
 
-# Create your views here.
+from .serializers import DomainSerializer, FlagSerializer
+
+
+class DomainsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        """
+        Register Domains in the system.
+        """
--- flags/views.py
+++ flags/views.py
@@ -3,8 +3,10 @@ from django.urls import reverse
 from django.views import View
 from django.http import HttpRequest
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from django.db.models import Q
 
-from .models import Domain
+from .models import Domain, Flag
 
 
 class DashboardView(LoginRequiredMixin, View):
@@ -17,8 +19,16 @@ class DashboardView(LoginRequiredMixin, View):
             - Charts data
             - latest domains
         """
+        all_domains = Domain.objects.all().count()
+        expired_domains = Domain.objects.filter(expire_at__lt=timezone.now()).count()
+        active_domains = Domain.objects.filter(expire_at__gt=timezone.now()).count()
+        summary = {
+            "domains": {"all": all_domains, "expired": expired_domains},
+            "flags": Flag.objects.all().count(),
+        }
+        chart = {"pie_data": [active_domains, expired_domains]}
         latest_domains = Domain.objects.all()[:5]
-        context = {"latest_domains": latest_domains}
+        context = {"latest_domains": latest_domains, "summary": summary, "chart": chart}
         return render(request, self.template, context)
 
 
@@ -34,7 +44,30 @@ class DomainListView(LoginRequiredMixin, View):
     template_name = "flags/domains.html"
 
     def get(self, request: HttpRequest, *args, **kwargs):
-        context = {}
+        """
+        Returns registered domains.
+            - It applies filter if status  or domain variable is found in the HTTP request
+        """
+        domains = Domain.objects.all().order_by("-created_at")[:50]
+
+        status = request.GET.get("status", "")
+        domain = request.GET.get("domain", "")
+
+        if status != "" or domain != "":
+            ACTIVE_STATUS = "ACTIVE"
+            q = Q()
+            if status != "":
+                query = (
+                    Q(expire_at__lt=timezone.now())
+                    if status == ACTIVE_STATUS
+                    else Q(expire_at__gt=timezone.now())
+                )
+                q &= query
+            if domain != "":
+                q &= Q(fqdn__icontains=domain)
+
+            domains = Domain.objects.filter(q).order_by("-created_at")[:50]
+        context = {"domains": domains}
         return render(request, self.template_name, context)
 
 
